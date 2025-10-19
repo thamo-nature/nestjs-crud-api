184e2851537fdd022d310a3da797e1d79d34b15c
Thamothiran Naturalist <29664661+thamo-nature@users.noreply.github.com>
2025-10-20 02:23:20 +0530

    Refactor CORS setup in bootstrap function
    Updated CORS configuration to use environment variable for origin.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  //app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:8000',
+  });  
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
